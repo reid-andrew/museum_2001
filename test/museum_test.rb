940d03c996cb9e5e0360e97584e31cc9e4fa77f7
Andrew Reid <areid.sj@gmail.com>
2020-02-19 10:10:37 -0700

    Write test for identifying lottery contestants

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -77,19 +77,19 @@ class MuseumTest < Minitest::Test
     assert_equal expected, @dmns.patrons_by_exhibit_interest
   end
 
-  # def test_it_identifies_lottery_contestants
-  #   @dmns.add_exhibit(@gems_and_minerals)
-  #   @dmns.add_exhibit(@dead_sea_scrolls)
-  #   @dmns.add_exhibit(@imax)
-  #   @patron_1.add_interest("Gems and Minerals")
-  #   @patron_1.add_interest("Dead Sea Scrolls")
-  #   @patron_2.add_interest("Dead Sea Scrolls")
-  #   @patron_3.add_interest("Dead Sea Scrolls")
-  #   @dmns.admit(@patron_1)
-  #   @dmns.admit(@patron_2)
-  #   @dmns.admit(@patron_3)
-  #
-  #   expected = []
-  #
-  # end
+  def test_it_identifies_lottery_contestants
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_2.add_interest("Dead Sea Scrolls")
+    @patron_3.add_interest("Dead Sea Scrolls")
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+    expected = [@patron_1, @patron_3]
+
+    assert_equal expected, @dmns.ticket_lottery_contestants(@dead_sea_scrolls)
+  end
 end
