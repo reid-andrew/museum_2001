d6be5a6f5a0bf9ede9c92d1b2cf625998dcacc07
Andrew Reid <areid.sj@gmail.com>
2020-02-19 09:32:24 -0700

    Update setup method on museum test class

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -10,6 +10,11 @@ class MuseumTest < Minitest::Test
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2 = Patron.new("Sally", 20)
+    @patron_2.add_interest("IMAX")
   end
 
   def test_it_exists
