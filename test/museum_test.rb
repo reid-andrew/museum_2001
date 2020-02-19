608ced868e73d4bafb260a74a016f8f93f424869
Andrew Reid <areid.sj@gmail.com>
2020-02-19 10:09:02 -0700

    Refactor setup again

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -11,8 +11,7 @@ class MuseumTest < Minitest::Test
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
-    @patron_1 = Patron.new("Bob", 20)
-
+    @patron_1 = Patron.new("Bob", 0)
     @patron_2 = Patron.new("Sally", 20)
     @patron_3 = Patron.new("Johnny", 5)
   end
@@ -77,4 +76,20 @@ class MuseumTest < Minitest::Test
     }
     assert_equal expected, @dmns.patrons_by_exhibit_interest
   end
+
+  # def test_it_identifies_lottery_contestants
+  #   @dmns.add_exhibit(@gems_and_minerals)
+  #   @dmns.add_exhibit(@dead_sea_scrolls)
+  #   @dmns.add_exhibit(@imax)
+  #   @patron_1.add_interest("Gems and Minerals")
+  #   @patron_1.add_interest("Dead Sea Scrolls")
+  #   @patron_2.add_interest("Dead Sea Scrolls")
+  #   @patron_3.add_interest("Dead Sea Scrolls")
+  #   @dmns.admit(@patron_1)
+  #   @dmns.admit(@patron_2)
+  #   @dmns.admit(@patron_3)
+  #
+  #   expected = []
+  #
+  # end
 end
