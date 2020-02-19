a7e49d52cf61c5477d40dc3253c3691d976ad1a1
Andrew Reid <areid.sj@gmail.com>
2020-02-19 09:29:49 -0700

    Write test museum adds exhibits

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -2,22 +2,31 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require 'mocha/minitest'
 require './lib/museum'
-require './lib/exhbits'
+require './lib/exhibit'
 
 class MuseumTest < Minitest::Test
   def setup
-    @museum = Museum.new("Denver Museum of Nature and Science")
+    @dmns = Museum.new("Denver Museum of Nature and Science")
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
   end
 
   def test_it_exists
-    assert_instance_of Museum, @museum
+    assert_instance_of Museum, @dmns
   end
 
   def test_it_has_attributes
     assert_equal  "Denver Museum of Nature and Science", @museum.name
-    assert_equal [], @museum.exhibits
+    assert_equal [], @dmns.exhibits
+  end
+
+  def test_it_adds_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    expected = [@gems_and_minerals, @dead_sea_scrolls, @imax]
+
+    assert_equal expected, @dmns.exhibits
   end
 end
