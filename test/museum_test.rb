6778510ea2f3901dd46557ad5d8de82032d5d4e2
Andrew Reid <areid.sj@gmail.com>
2020-02-19 09:42:19 -0700

    Define recommend exhibits method

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -37,6 +37,10 @@ class MuseumTest < Minitest::Test
   end
 
   def test_it_recommends_exhibits
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@imax)
+
     assert_equal [@dead_sea_scrolls, @gems_and_minerals], @dmns.recommend_exhibits(@patron_1)
     assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
   end
