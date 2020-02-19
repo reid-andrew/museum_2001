443700eda695ec9b18c30d96a9c2a48731588704
Andrew Reid <areid.sj@gmail.com>
2020-02-19 10:37:41 -0700

    Write test for announce lottery winner

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -109,4 +109,26 @@ class MuseumTest < Minitest::Test
     assert_instance_of String, @dmns.draw_lottery_winner(@dead_sea_scrolls)
     refute_equal @patron_2.name, @dmns.draw_lottery_winner(@dead_sea_scrolls)
   end
+
+  def test_it_announces_lottery_winner
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_2.add_interest("Dead Sea Scrolls")
+    @patron_3.add_interest("Dead Sea Scrolls")
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+
+    expected1 = "No winners for this lottery"
+    assert_equal expected1, @dmns.announce_lottery_winner(@gems_and_minerals)
+
+    exhibit1 = mock
+    exhibit1.expects(:draw_lottery_winner).returns("Bob")
+
+    expected2 = "Bob has won the Dead Sea Scrolls exhibit lottery"
+    assert_equal expected2, @dmns.announce_lottery_winner(@dead_sea_scrolls)
+  end
 end
