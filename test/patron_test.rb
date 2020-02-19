caaea205ece3f3b9ca0c8b43355fcc4d3ac85250
Andrew Reid <areid.sj@gmail.com>
2020-02-19 09:13:53 -0700

    Write patron has attributes test

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -5,10 +5,15 @@ require './lib/patron'
 
 class PatronTest < Minitest::Test
   def setup
-    @patron = Patron.new
+    @patron = Patron.new("Bob", 20)
   end
 
   def test_it_exists
     assert_instance_of Patron, @patron
+  end
+
+  def test_it_has_attributes
+    assert_equal "Bob", @patron.name
+    assert_equal 20, @patron.spending_money
   end
 end
